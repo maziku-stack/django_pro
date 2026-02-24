60f54e14a13eaaad4f9ecbcc2c11ae7ecef8c6f7
immanuel <jumamaziku77@gmail.com>
2026-02-24 00:32:43 -0800

    changes

--- E_commerce/STORE/urls.py
+++ E_commerce/STORE/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-
-from E_commerce.STORE import views
+from . import views
 
 
 urlpatterns = [
--- E_commerce/STORE/views.py
+++ E_commerce/STORE/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpRequest
 from django.shortcuts import render
-from . views import store
+from . import views
 # Create your views here.
 def store(request):
         return render(request,'store.html',name="store") 
