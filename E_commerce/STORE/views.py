e633d5950c5e0bc13950b46eaecba9c28cbee54c
immanuel <jumamaziku77@gmail.com>
2026-02-24 03:28:06 -0800

    changes

--- E_commerce/STORE/views.py
+++ E_commerce/STORE/views.py
@@ -1,6 +1,14 @@
 from django.http import HttpRequest
 from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
 from . import views
 # Create your views here.
 def store(request):
-        return render(request,'store.html',name="store") 
+        try:
+            # Simulating an attempt to retrieve an object that might not exist
+            # In a real app, this would be replaced with actual logic to retrieve data
+             render(request,'store.html') 
+        except ObjectDoesNotExist:
+            # Handle the case where the object does not exist
+             render(request, 'store.html', {'error': 'Object does not exist'})
+        
