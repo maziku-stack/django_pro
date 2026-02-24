32c5dbbea80fc134fb7b0e291b301cb24a4aa5d4
immanuel <jumamaziku77@gmail.com>
2026-02-24 05:02:21 -0800

    comit

--- E_commerce/STORE/views.py
+++ E_commerce/STORE/views.py
@@ -1,8 +1,5 @@
 
 from django.shortcuts import render
-from django.core.exceptions import ObjectDoesNotExist
-from . import views
-from django.http import  HttpResponse
 # Create your views here.
 def store(request):
         return render(request,"store.html")
