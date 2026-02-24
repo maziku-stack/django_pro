b58124ab58ea88d4d439250f1755c3d7cbca3d60
immanuel <jumamaziku77@gmail.com>
2026-02-24 05:00:10 -0800

    q!

--- E_commerce/E_commerce/settings.py
+++ E_commerce/E_commerce/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'STORE',
-    'TAGS',
-    'LIKE'
+    'Store',
+    'Tags',
+    'Likes'
 ]
 
 MIDDLEWARE = [
